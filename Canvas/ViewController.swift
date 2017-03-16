783f8d6f5c33518770841d9b0e905f6a2de13d63
Sang Saephan <sangsaephan510@yahoo.com>
2017-03-15 23:40:40 -0700

    Finish Canvas Lab

--- Canvas/ViewController.swift
+++ Canvas/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var trayView: UIView!
+    var trayOriginalCenter: CGPoint!
+    var trayCenterWhenOpen: CGPoint!
+    var trayCenterWhenClosed: CGPoint!
+    var newCreatedFace: UIImageView!
+    var faceOriginalCenter: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        trayCenterWhenOpen = trayView.center
+        trayCenterWhenClosed = CGPoint(x: trayView.center.x, y: trayView.center.y + 175)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,49 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTrayPanGesture(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: view)
+        let translation = sender.translation(in: view)
+        let velocity = sender.velocity(in: view)
+         
+        if sender.state == .began {
+            trayOriginalCenter = trayView.center
+        } else if sender.state == .changed {
+            //trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+            if velocity.y > 0 {
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.trayView.center = self.trayCenterWhenClosed
+                })
+            }
+            if velocity.y < 0 {
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.trayView.center = self.trayCenterWhenOpen
+                })
+            }
+        } else if sender.state == .ended {
+            print("Ended")
+        }
+    }
+    
+    @IBAction func onSmileyPanGesture(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: view)
+        let translation = sender.translation(in: view)
+        
+        if sender.state == .began {
+            var imageView = sender.view as? UIImageView
+            self.newCreatedFace = UIImageView(image: imageView?.image)
+            self.view.addSubview(self.newCreatedFace)
+            self.newCreatedFace.center = (imageView?.center)!
+            
+            let faceCenter = self.newCreatedFace.center
+            self.newCreatedFace.center = CGPoint(x: faceCenter.x, y: faceCenter.y + trayView.frame.origin.y)
+            faceOriginalCenter = newCreatedFace.center
+        } else if sender.state == .changed {
+            newCreatedFace.center = CGPoint(x: faceOriginalCenter.x + translation.x, y: faceOriginalCenter.y + translation.y)
+        } else if sender.state == .ended {
+            
+        }
+    }
 
 }
 
---
+++ README.md
@@ -0,0 +1,7 @@
+# Canvas
+
+##Description:
+Using views, gesture recognizers, and animations to build a tray of smiley faces that can be dragged onto a canvas.
+
+##Demo:
+![](Canvas.gif)
